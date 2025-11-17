ab7f38184aa46db427c2f4f382e181ba7a2a1d93
Jo-Developer40 <juergen.schulz@me.com>
2025-11-17 17:18:17 +0100

    Initial Commit

---
+++ HoldDownButton4/HoldDownButton4App.swift
@@ -0,0 +1,17 @@
+//
+//  HoldDownButton4App.swift
+//  HoldDownButton4
+//
+//  Created by Juergen on 17.11.25.
+//
+
+import SwiftUI
+
+@main
+struct HoldDownButton4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
